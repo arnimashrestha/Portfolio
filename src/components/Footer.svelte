44179d4d2795f187b610930969193142d1b87808
arnimashrestha” <a7shrest@uwaterloo.ca>
2024-01-14 20:43:01 -0500

    yportfolio

---
+++ src/components/Footer.svelte
@@ -0,0 +1,66 @@
+<footer class= "py-20 sm:py bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+    <p class= "px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class= "flex flex-col gap-4 items-center justify-center">
+        <p><b class= "pr-2">Email: </b>a7shrest@uwaterloo.ca</p>
+        <p><b class= "pr-2">GitHub</b>
+            <a class= "text-violet-400" target= "_blank" href= "https://github.com/arnimashrestha?tab=repositories">
+                arnimashrestha <sup
+                ><span class= "text-xs scale-75 pl-0.5">
+                    <i
+                    class= "fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                    />
+
+                </span>
+                    
+                </sup>
+
+            </a>
+        </p>
+
+        <p><b class= "pr-2">LinkedIn</b>
+            <a class= "text-violet-400" target= "_blank" href= "https://www.linkedin.com/in/arnimashrestha/">
+                arnimashrestha <sup
+                ><span class= "text-xs scale-75 pl-0.5">
+                    <i
+                    class= "fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                    />
+
+                </span>
+                    
+                </sup>
+            </a>
+        </p>
+
+        <p><b class= "pr-2">Software Resume</b>
+            <a class= "text-violet-400" target= "_blank" href= "https://docs.google.com/document/d/1oqxqrtiy_e5H5bvblRDE1-kLRZzdW7aUlVydYm3a0sE/edit?usp=sharing">
+                Click here! <sup
+                ><span class= "text-xs scale-75 pl-0.5">
+                    <i
+                    class= "fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                    />
+                </span>
+                    
+                </sup>
+            </a>
+        </p>
+        <p><b class= "pr-2">Mechanical Resume</b>
+            <a class= "text-violet-400" target= "_blank" href= "https://docs.google.com/document/d/1HC-YiqMKXA1AcJrCLi15GrvBprNscf11QEgQj1s014w/edit?usp=sharing">
+                Click here! <sup
+                ><span class= "text-xs scale-75 pl-0.5">
+                    <i
+                    class= "fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                    />
+                </span>
+                    
+                </sup>
+            </a>
+        </p>
+
+
+
+    </div>
+
+
+</footer>
